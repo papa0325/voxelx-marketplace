811c64d7f1df2430e9260300556205b18db94121
cryptoDev <93045591+papa0325@users.noreply.github.com>
2022-01-31 22:34:53 +0300

    public

--- public/js/countdown-custom.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-        $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2021, 12-1, 10, 8)}); // year, month, date, hour
-        });
-});		
-
